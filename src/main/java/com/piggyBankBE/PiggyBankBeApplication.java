2f83a7cb4dd1f6234543183150384dbc58f96938
ZMichaelaK <korn_miku@hotmail.com>
2024-05-07 10:49:19 +0100

    initial commit

---
+++ src/main/java/com/piggyBankBE/PiggyBankBeApplication.java
@@ -0,0 +1,13 @@
+package com.piggyBankBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiggyBankBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiggyBankBeApplication.class, args);
+	}
+
+}
